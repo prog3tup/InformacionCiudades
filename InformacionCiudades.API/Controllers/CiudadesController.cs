442cca9fa5a491067acbedc6452237af79a8a4d4
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2022-05-07 16:45:06 -0300

    Clase 5

---
+++ InformacionCiudades.API/Controllers/CiudadesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/ciudades")]
+    public class CiudadesController : ControllerBase //controller
+                                                     //deriva de ControllerBase, usamos ControllerBase para
+                                                     //no traer todas las cosas que no vamos a usar
+    {
+        [HttpGet]
+        public JsonResult GetCiudades()
+        {
+            return new JsonResult(
+            new List<object>
+            {
+                    new { id = 1, name = "Rosario"},
+                    new { id = 2, name = "Buenos Aires"}
+            });
+        }
+
+        [HttpGet("metodo2")]
+        public JsonResult GetCiudad()
+        {
+            return new JsonResult(new List<string>
+            {
+                "Santa Fe",
+                "Funes"
+            });
+        }
+    }
+}
