17886bdcf50968b5cc40343c35a159c6ce781813
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2022-05-19 19:58:11 -0300

    Crear DB Context

---
+++ InformacionCiudades.API/DBContexts/InformacionCiudadesContext.cs
@@ -0,0 +1,16 @@
+﻿using InformacionCiudades.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InformacionCiudades.API.DBContexts
+{
+    public class InformacionCiudadesContext : DbContext
+    {
+        public DbSet<Ciudad> Ciudades { get; set; } //lo que hagamos con LINQ sobre estos DbSets lo va a transformar en consultas SQL
+        public DbSet<PuntoDeInteres> PuntosDeInteres { get; set; } //Los warnings los podemos obviar porque DbContext se encarga de eso.
+
+        public InformacionCiudadesContext(DbContextOptions<InformacionCiudadesContext> options) : base(options) //Acá estamos llamando al constructor de DbContext que es el que acepta las opciones
+        {
+
+        }
+    }
+}
