563dff6947e6e95729a37eec095c1e14d594268b
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2022-05-19 19:57:46 -0300

    crear entidades

---
+++ InformacionCiudades.API/Entities/Ciudad.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InformacionCiudades.API.Entities
+{
+    public class Ciudad
+    {
+        [Key] //Esto es opcional si se sigue la convención
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //También por convención no hace falta. Identity genera un nuevo Id por cada creación.
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [MaxLength(200)]
+        public string? Descripcion { get; set; }
+
+        public ICollection<PuntoDeInteres> PuntosDeInteres { get; set; } = new List<PuntoDeInteres>(); //Lo seteamos a una nueva colección para evitar que retorne un null en algún momento de la ejecución.
+
+        public Ciudad(string nombre)
+        {
+            Nombre = nombre.Trim();
+        }
+    }
+}
---
+++ InformacionCiudades.API/Entities/PuntoDeInteres.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InformacionCiudades.API.Entities
+{
+    public class PuntoDeInteres
+    {
+        [Key] //Esto es opcional si se sigue la convención
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //También por convención no hace falta. Identity genera un nuevo Id por cada creación.
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+        //public string? Descripcion { get; set; } Lo dejamos para después.
+
+        public Ciudad? Ciudad { get; set; }
+        public PuntoDeInteres(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+}
